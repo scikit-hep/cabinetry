3f0049b574a4e8e57a5752f0ec890a681114e239
alexander-held <45009355+alexander-held@users.noreply.github.com>
2020-05-07 15:55:13 -0400

    build: adding setup.py and changing to src layout (#3)
    * adding setup.py
    * adopting src layout
    * testing installed package in CI

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="cabinetry",
+    version="0.0.1",
+    author="cabinetry developers",
+    description="design and steer profile likelihood fits",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD 3-Clause",
+    url="https://github.com/alexander-held/cabinetry",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["numpy", "pyyaml"],
+)
