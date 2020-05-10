7d0fe00d0cc1aab3b2aad993eb933fd0b677f2ab
alexander-held <45009355+alexander-held@users.noreply.github.com>
2020-05-10 15:15:55 -0400

    feat: basic unconstrained fitting (#8)

--- example.py
+++ example.py
@@ -18,34 +18,30 @@ if __name__ == "__main__":
         raise SystemExit
 
     # import example config file
-    fit_configuration = cabinetry.config.read("config_example.yml")
-    cabinetry.config.print_overview(fit_configuration)
+    fit_config = cabinetry.config.read("config_example.yml")
+    cabinetry.config.print_overview(fit_config)
 
     # create template histograms
     input_folder = "util/ntuples"
-    histogram_folder = "histograms/"
+    histo_folder = "histograms/"
     cabinetry.template_builder.create_histograms(
-        fit_configuration, histogram_folder, only_nominal=True, method="uproot"
+        fit_config, histo_folder, only_nominal=True, method="uproot"
     )
 
     # perform histogram post-processing
-    cabinetry.template_postprocessor.run(fit_configuration, histogram_folder)
+    cabinetry.template_postprocessor.run(fit_config, histo_folder)
 
     # build a workspace and save to file
     workspace_folder = "workspaces/"
     workspace_name = "example_workspace"
-    ws = cabinetry.workspace.build(fit_configuration, histogram_folder)
+    ws = cabinetry.workspace.build(fit_config, histo_folder)
     cabinetry.workspace.save(ws, workspace_folder, workspace_name)
 
     # run a fit
-    pass
+    cabinetry.fit.fit(ws)
 
     # visualize some results
     figure_folder = "figures/"
     cabinetry.visualize.data_MC(
-        fit_configuration,
-        histogram_folder,
-        figure_folder,
-        prefit=True,
-        method="matplotlib",
+        fit_config, histo_folder, figure_folder, prefit=True, method="matplotlib",
     )
