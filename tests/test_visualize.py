73240f91a3e0778cd8872dcf5a133e9ac03190fd
Alexander Held <alexander.held@cern.ch>
2020-07-01 18:42:29 +0200

    using fixture to parametrize

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,8 +1,16 @@
+import pytest
+
 from cabinetry import visualize
 
 
-def test__build_figure_name():
-    assert visualize._build_figure_name("SR", True) == "SR_prefit.pdf"
-    assert visualize._build_figure_name("SR", False) == "SR_postfit.pdf"
-    assert visualize._build_figure_name("SR 1", True) == "SR-1_prefit.pdf"
-    assert visualize._build_figure_name("SR 1", False) == "SR-1_postfit.pdf"
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (("SR", True), "SR_prefit.pdf"),
+        (("SR", False), "SR_postfit.pdf"),
+        (("SR 1", True), "SR-1_prefit.pdf"),
+        (("SR 1", False), "SR-1_postfit.pdf"),
+    ],
+)
+def test__build_figure_name(test_input, expected):
+    assert visualize._build_figure_name(*test_input) == expected
