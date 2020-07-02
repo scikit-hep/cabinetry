abe2c90ec1ead76c4f453ac5254ceaa5baf08755
alexander-held <45009355+alexander-held@users.noreply.github.com>
2020-07-02 15:04:54 +0200

    test: visualization (#40)
    * adding tests for data-MC plot with matplotlib backend and data aggregation for plotting

--- tests/contrib/test_histogram_drawing.py
+++ tests/contrib/test_histogram_drawing.py
@@ -1,6 +1,8 @@
+from matplotlib.testing.compare import compare_images
 import numpy as np
 
 from cabinetry.contrib import histogram_drawing
+from cabinetry import histo
 
 
 def test__total_yield_uncertainty():
@@ -8,4 +10,26 @@ def test__total_yield_uncertainty():
     expected_uncertainties = [0.31622777, 0.28284271, 0.14142136]
     assert np.allclose(
         histogram_drawing._total_yield_uncertainty(sumw2_list), expected_uncertainties
+    )
+
+
+def test_data_MC(tmp_path):
+    fname = tmp_path / "subdir" / "fig.pdf"
+    bg_hist = histo.to_dict(
+        np.asarray([12.5, 14]), np.asarray([0.4, 0.5]), np.asarray([1, 2, 3])
+    )
+    sig_hist = histo.to_dict(
+        np.asarray([2, 5]), np.asarray([0.1, 0.2]), np.asarray([1, 2, 3])
+    )
+    data_hist = histo.to_dict(
+        np.asarray([13, 15]), np.asarray([3.61, 3.87]), np.asarray([1, 2, 3])
+    )
+    histo_dict_list = [
+        {"label": "Background", "isData": False, "hist": bg_hist, "variable": "x"},
+        {"label": "Signal", "isData": False, "hist": sig_hist, "variable": "x"},
+        {"label": "Data", "isData": True, "hist": data_hist, "variable": "x"},
+    ]
+    histogram_drawing.data_MC_matplotlib(histo_dict_list, fname)
+    assert (
+        compare_images("tests/contrib/reference/ref_data_MC.pdf", str(fname), 0) is None
     )
--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,8 +1,66 @@
+from unittest import mock
+
+import pytest
+
 from cabinetry import visualize
+from cabinetry.contrib import histogram_drawing
 
 
-def test__build_figure_name():
-    assert visualize._build_figure_name("SR", True) == "SR_prefit.pdf"
-    assert visualize._build_figure_name("SR", False) == "SR_postfit.pdf"
-    assert visualize._build_figure_name("SR 1", True) == "SR-1_prefit.pdf"
-    assert visualize._build_figure_name("SR 1", False) == "SR-1_postfit.pdf"
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (("SR", True), "SR_prefit.pdf"),
+        (("SR", False), "SR_postfit.pdf"),
+        (("SR 1", True), "SR-1_prefit.pdf"),
+        (("SR 1", False), "SR-1_postfit.pdf"),
+    ],
+)
+def test__build_figure_name(test_input, expected):
+    assert visualize._build_figure_name(*test_input) == expected
+
+
+@mock.patch("cabinetry.contrib.histogram_drawing.data_MC_matplotlib")
+@mock.patch("cabinetry.visualize.histo.load_from_config", return_value=({}, ""))
+def test_data_MC(mock_load, mock_draw, tmp_path):
+    """contrib.histogram_drawing is only imported depending on the keyword argument,
+    so the patch works differently from the patch of load_from_config
+    Generally it seems like following the path to the module is preferred, but that
+    does not work for the `data_MC_matplotlib` case. For some information see also
+    https://docs.python.org/3/library/unittest.mock.html#where-to-patch
+    """
+    config = {
+        "Regions": [{"Name": "reg_1", "Variable": "x"}],
+        "Samples": [{"Name": "sample_1"}],
+    }
+
+    visualize.data_MC(config, tmp_path, tmp_path, prefit=True, method="matplotlib")
+
+    # the call_args_list contains calls (outer round brackets), first filled with
+    # arguments (inner round brackets) and then keyword arguments
+    assert mock_load.call_args_list == [
+        (
+            (
+                tmp_path,
+                {"Name": "sample_1"},
+                {"Name": "reg_1", "Variable": "x"},
+                {"Name": "nominal"},
+            ),
+            {"modified": True},
+        )
+    ]
+    assert mock_draw.call_args_list == [
+        (
+            (
+                [{"label": "sample_1", "isData": False, "hist": {}, "variable": "x"}],
+                tmp_path / "reg_1_prefit.pdf",
+            ),
+        )
+    ]
+
+    # other plotting method
+    with pytest.raises(Exception) as e_info:
+        visualize.data_MC(config, tmp_path, tmp_path, prefit=True, method="unknown")
+
+    # postfit
+    with pytest.raises(Exception) as e_info:
+        visualize.data_MC(config, tmp_path, tmp_path, prefit=False, method="matplotlib")
