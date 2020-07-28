4fa9b190f12e02bfbd8e7790e06f1f08e62dc759
Alexander Held <alexander.held@cern.ch>
2020-07-28 11:32:06 +0200

    try to break CI for testing

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     url="https://github.com/alexander-held/cabinetry",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"cabinetry": ["py.typed", "schemas/config.json"]},
+    package_data={"cabinetry": ["py.typed"]},
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3.6",
