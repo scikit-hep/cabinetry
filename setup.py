f39096ebdcd1f7f35cafa4e5d82fda585601c9b6
Alexander Held <alexander.held@cern.ch>
2020-07-31 15:28:59 +0200

    removing boost_histogram version restriction

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "pyyaml",
         "pyhf>=0.5.0",
         "iminuit>1.4.0",
-        "boost_histogram!=0.10.1",
+        "boost_histogram",
         "jsonschema",
     ],
     extras_require=extras_require,
