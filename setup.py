d63daa5ec3ea86119690cbfda56fad546755250b
Alexander Held <alexander.held@cern.ch>
2020-09-03 13:25:14 +0200

    moving metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 extras_require = {"contrib": ["matplotlib", "uproot", "uproot4", "awkward1"]}
 extras_require["test"] = sorted(
@@ -36,40 +36,6 @@ extras_require["develop"] = sorted(
 )
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
 setup(
-    name="cabinetry",
-    version="0.1.1",
-    author="Alexander Held",
-    description="design and steer profile likelihood fits",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="BSD 3-Clause",
-    url="https://github.com/alexander-held/cabinetry",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    package_data={"cabinetry": ["py.typed", "schemas/config.json"]},
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
-    python_requires=">=3.6",
-    install_requires=[
-        "numpy",
-        "pyyaml",
-        "pyhf>=0.5.1",  # fixed parameter bookkeeping #989
-        "iminuit>1.4.0",
-        "boost_histogram",
-        "jsonschema",
-        "click",
-    ],
     extras_require=extras_require,
 )
