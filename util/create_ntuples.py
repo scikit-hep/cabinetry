481b62c333312b5318fc5d5c1ed025766c2d9831
Alexander Held <alexander.held@cern.ch>
2020-09-07 17:06:24 +0200

    disabling plot by default

--- util/create_ntuples.py
+++ util/create_ntuples.py
@@ -169,7 +169,7 @@ def plot_distributions(data, weights, labels, pseudodata, bins):
     plt.savefig("stacked.png", dpi=200)
 
 
-def run(output_directory):
+def run(output_directory, visualize=False):
     # configuration
     num_events = 5000
     yield_s = 125
@@ -213,9 +213,10 @@ def run(output_directory):
     np.testing.assert_allclose(w_read, weights)
     np.testing.assert_allclose(pd_read, pseudodata)
 
-    # visualize results
-    bins = np.linspace(0, 1200, 24 + 1)
-    plot_distributions(d_read, w_read, l_read, pseudodata, bins)
+    if visualize:
+        # visualize results
+        bins = np.linspace(0, 1200, 24 + 1)
+        plot_distributions(d_read, w_read, l_read, pseudodata, bins)
 
 
 if __name__ == "__main__":
