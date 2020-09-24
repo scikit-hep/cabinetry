a45e9abecd3700dfb086164c791cdd51636b7bbc
Alexander Held <alexander.held@cern.ch>
2020-09-24 16:51:04 +0200

    better comment

--- src/cabinetry/model_utils.py
+++ src/cabinetry/model_utils.py
@@ -132,7 +132,7 @@ def get_prefit_uncertainties(model: pyhf.pdf.Model) -> np.ndarray:
     """
     pre_fit_unc = []  # pre-fit uncertainties for parameters
     for parameter in model.config.par_order:
-        # obtain pre-fit uncertainty for constrained and non-fixed parameters
+        # obtain pre-fit uncertainty for constrained, non-fixed parameters
         if (
             model.config.param_set(parameter).constrained
             and not model.config.param_set(parameter).fixed
