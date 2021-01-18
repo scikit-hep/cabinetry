8742f737d7ec0dcd2397940bcf7a16ea14cd67aa
Alexander Held <alexander.held@cern.ch>
2021-01-18 17:24:33 +0100

    add back new logic

--- src/cabinetry/contrib/matplotlib_visualize.py
+++ src/cabinetry/contrib/matplotlib_visualize.py
@@ -71,12 +71,11 @@ def data_MC(
     bin_width = bin_right_edges - bin_left_edges
     bin_centers = 0.5 * (bin_left_edges + bin_right_edges)
     # center data visually in bins if horizontal log scale is used
-    bin_centers_data = bin_centers
-    # (
-    #     np.power(10, 0.5 * (np.log10(bin_left_edges * bin_right_edges)))
-    #     if log_scale_x
-    #     else bin_centers
-    # )
+    bin_centers_data = (
+        np.power(10, 0.5 * (np.log10(bin_left_edges * bin_right_edges)))
+        if log_scale_x
+        else bin_centers
+    )
     mc_containers = []
     for mc_sample_yield in mc_histograms_yields:
         mc_container = ax1.bar(
