d84991c8a5faead4db8aab483784baa4eabb35c5
Alexander Held <alexander.held@cern.ch>
2021-06-09 23:54:49 +0200

    only run typeguard in Python 3.8+, update typeguard version requirements

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras_require["test"] = sorted(
             "mypy",
             "types-tabulate",
             "types-PyYAML",
-            "typeguard!=2.11.*,!=2.12.*",  # NamedTuple incompatibility in Python 3.7
+            "typeguard>=2.12.1",  # click 8.0 compatibility
             "black",
         ]
     )
