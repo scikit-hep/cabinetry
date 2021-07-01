2ec6735f272091027d6bd64c329d8cd4d374a576
Alexander Held <alexander.held@cern.ch>
2021-07-01 21:30:46 +0200

    check that file was saved

--- tests/contrib/test_matplotlib_visualize.py
+++ tests/contrib/test_matplotlib_visualize.py
@@ -17,7 +17,8 @@ def test__save_figure(tmp_path):
     fig = plt.figure()
     fname = tmp_path / "fig.pdf"
     matplotlib_visualize._save_figure(fig, fname)
-    assert len(plt.get_fignums()) == 1
+    assert fname.is_file()  # file was saved
+    assert len(plt.get_fignums()) == 1  # figure is open
 
     fig = plt.figure()  # new figure to test closing
     matplotlib_visualize._save_figure(fig, fname, close_figure=True)
