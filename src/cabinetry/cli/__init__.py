77045b16633a1151a3163fac6ba4ac29bfcf7e32
Alexander Held <alexander.held@cern.ch>
2021-08-10 12:39:18 +0200

    codefactor fix

--- src/cabinetry/cli/__init__.py
+++ src/cabinetry/cli/__init__.py
@@ -135,7 +135,8 @@ def fit(
         cabinetry_visualize.pulls(fit_results, figure_folder=figfolder)
     if corrmat:
         cabinetry_visualize.correlation_matrix(fit_results, figure_folder=figfolder)
-    pass  # fixes coverage, see https://github.com/nedbat/coveragepy/issues/198
+    # pass fixes coverage, see https://github.com/nedbat/coveragepy/issues/198
+    pass  # noqa
 
 
 @click.command()
