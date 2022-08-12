fa1b7a2265719301bbbd3c1a38f8ead5f50f90f0
Alexander Held <45009355+alexander-held@users.noreply.github.com>
2022-08-12 21:21:36 +0200

    feat: save yield tables (#351)
    * support automatic saving of yield tables in customizable formats

--- tests/test_tabulate.py
+++ tests/test_tabulate.py
@@ -72,6 +72,73 @@ def test__yields_per_bin(example_spec_multibin, example_spec_with_background, ca
     ]
 
 
+def test__save_tables(tmp_path):
+    table_dict = {
+        "yields_per_bin": [
+            {
+                "sample": "Background",
+                "Signal_region\nbin 1": "111.00",
+                "Signal_region\nbin 2": "116.00",
+            },
+            {
+                "sample": "Signal",
+                "Signal_region\nbin 1": "1.00",
+                "Signal_region\nbin 2": "5.00",
+            },
+            {
+                "sample": "total",
+                "Signal_region\nbin 1": "112.00 ± 10.00",
+                "Signal_region\nbin 2": "121.00 ± 11.00",
+            },
+            {
+                "sample": "data",
+                "Signal_region\nbin 1": "112.00",
+                "Signal_region\nbin 2": "123.00",
+            },
+        ],
+        "yields_per_channel": [
+            {"sample": "Background", "Signal_region": "227.00"},
+            {"sample": "Signal", "Signal_region": "6.00"},
+            {"sample": "total", "Signal_region": "233.00 ± 17.00"},
+            {"sample": "data", "Signal_region": "235.00"},
+        ],
+    }
+
+    tabulate._save_tables(table_dict, tmp_path, "abc", "simple")
+    fname_bin = tmp_path / "yields_per_bin_abc.txt"
+    fname_channel = tmp_path / "yields_per_channel_abc.txt"
+    assert fname_bin.is_file()  # bin table saved
+    assert fname_channel.is_file()  # channel table saved
+    assert fname_bin.read_text() == (
+        "sample      Signal_region    Signal_region\n"
+        "            bin 1            bin 2\n"
+        "----------  ---------------  ---------------\n"
+        "Background  111.00           116.00\n"
+        "Signal      1.00             5.00\n"
+        "total       112.00 ± 10.00   121.00 ± 11.00\n"
+        "data        112.00           123.00\n"
+    )
+    assert fname_channel.read_text() == (
+        "sample      Signal_region\n"
+        "----------  ---------------\n"
+        "Background  227.00\n"
+        "Signal      6.00\n"
+        "total       233.00 ± 17.00\n"
+        "data        235.00\n"
+    )
+
+    # suffix for latex, modification of newlines in header
+    tabulate._save_tables(table_dict, tmp_path, "abc", "latex")
+    fname_bin = tmp_path / "yields_per_bin_abc.tex"
+    assert fname_bin.is_file()
+    assert fname_bin.read_text().split("&")[1] == " Signal\\_region, bin 1   "
+
+    # unchanged suffix for other formats
+    tabulate._save_tables(table_dict, tmp_path, "abc", "html")
+    fname_bin = tmp_path / "yields_per_bin_abc.html"
+    assert fname_bin.is_file()
+
+
 def test__yields_per_channel(
     example_spec_multibin, example_spec_with_background, caplog
 ):
@@ -121,6 +188,7 @@ def test__yields_per_channel(
     ]
 
 
+@mock.patch("cabinetry.tabulate._save_tables")
 @mock.patch(
     "cabinetry.tabulate._yields_per_channel",
     return_value=[{"yields_per_channel": None}],
@@ -130,7 +198,9 @@ def test__yields_per_channel(
 )
 @mock.patch("cabinetry.model_utils._filter_channels", side_effect=[["SR"], [], ["SR"]])
 @mock.patch("cabinetry.model_utils._data_per_channel", return_value=[[12.0]])
-def test_yields(mock_data, mock_filter, mock_bin, mock_channel, example_spec, caplog):
+def test_yields(
+    mock_data, mock_filter, mock_bin, mock_channel, mock_save, example_spec, caplog
+):
     # the return values of the functions producing the actual tables are hardcoded above
     # to be able to check that they are correctly propagated to the output
     caplog.set_level(logging.DEBUG)
@@ -145,6 +215,7 @@ def test_yields(mock_data, mock_filter, mock_bin, mock_channel, example_spec, ca
         ((model, [[[10.0]]], [[0.3]], [[12.0]], ["SR"], "pred"), {})
     ]
     assert mock_channel.call_count == 0
+    assert mock_save.call_count == 1
     assert table_dict == {"yields_per_bin": [{"yields_per_bin": None}]}
 
     # no table to produce (does not call _filter_channels)
@@ -162,10 +233,13 @@ def test_yields(mock_data, mock_filter, mock_bin, mock_channel, example_spec, ca
     assert mock_channel.call_count == 0
     assert table_dict == {}
 
-    # yields per channel, not per bin
-    table_dict = tabulate.yields(model_pred, data, per_bin=False, per_channel=True)
+    # yields per channel, not per bin, do not save tables
+    table_dict = tabulate.yields(
+        model_pred, data, per_bin=False, per_channel=True, save_tables=False
+    )
     assert mock_bin.call_count == 1  # one call from before
     assert mock_channel.call_args_list == [
         ((model, [[10.0]], [0.3], [12.0], ["SR"], "pred"), {})
     ]
     assert table_dict == {"yields_per_channel": [{"yields_per_channel": None}]}
+    assert mock_save.call_count == 1  # one call from before, no new call
