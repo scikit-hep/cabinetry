c005bc2b8969f5c13db60c89f007eb640a4b15f2
Alexander Held <45009355+alexander-held@users.noreply.github.com>
2023-01-16 18:44:34 +0100

    chore: fix editor autocompletion (#386)
    * fixed editor autocompletion by appropriately importing cabinetry components
    * added top-level __all__
    * updated pre-commit

--- src/cabinetry/__init__.py
+++ src/cabinetry/__init__.py
@@ -1,16 +1,38 @@
 """The cabinetry library."""
 
 import logging
+from typing import List
 
-import cabinetry.configuration  # noqa: F401
-import cabinetry.fit  # noqa: F401
-import cabinetry.model_utils  # noqa: F401
-import cabinetry.route  # noqa: F401
-import cabinetry.smooth  # noqa: F401
-import cabinetry.tabulate  # noqa: F401
-import cabinetry.templates  # noqa: F401
-import cabinetry.visualize  # noqa: F401
-import cabinetry.workspace  # noqa: F401
+import cabinetry.configuration as configuration  # noqa: F401
+import cabinetry.fit as fit  # noqa: F401
+import cabinetry.histo as histo  # noqa: F401
+import cabinetry.model_utils as model_utils  # noqa: F401
+import cabinetry.route as route  # noqa: F401
+import cabinetry.smooth as smooth  # noqa: F401
+import cabinetry.tabulate as tabulate  # noqa: F401
+import cabinetry.templates as templates  # noqa: F401
+import cabinetry.visualize as visualize  # noqa: F401
+import cabinetry.workspace as workspace  # noqa: F401
+
+
+__all__ = [
+    "__version__",
+    "set_logging",
+    "configuration",
+    "fit",
+    "histo",
+    "model_utils",
+    "route",
+    "smooth",
+    "tabulate",
+    "templates",
+    "visualize",
+    "workspace",
+]
+
+
+def __dir__() -> List[str]:
+    return __all__
 
 
 __version__ = "0.5.1"
