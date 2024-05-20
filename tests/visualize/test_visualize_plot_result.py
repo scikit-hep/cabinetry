891fdb0305a005d8c8111be13f896efc5e59ad5f
Alexander Held <45009355+alexander-held@users.noreply.github.com>
2024-05-20 19:42:43 +0200

    test: update likelihood scan reference visualization for latest matplotlib (#476)
    * update legend positioning in likelihood scan visualization test reference
    * update test to handle expected failure in Python 3.8

--- tests/visualize/test_visualize_plot_result.py
+++ tests/visualize/test_visualize_plot_result.py
@@ -1,8 +1,10 @@
+import sys
 from unittest import mock
 
 import matplotlib.pyplot as plt
 from matplotlib.testing.compare import compare_images
 import numpy as np
+import pytest
 
 from cabinetry.visualize import plot_result
 
@@ -117,6 +119,10 @@ def test_ranking(tmp_path):
     plt.close("all")
 
 
+@pytest.mark.xfail(
+    sys.version_info <= (3, 9),
+    reason="legend positioning in Python 3.8 with older matplotlib, see cabinetry#476",
+)
 def test_scan(tmp_path):
     fname = tmp_path / "fig.png"
     par_name = "a"
@@ -128,12 +134,18 @@ def test_scan(tmp_path):
     fig = plot_result.scan(
         par_name, par_mle, par_unc, par_vals, par_nlls, figure_path=fname
     )
-    assert compare_images("tests/visualize/reference/scan.png", str(fname), 0) is None
+    # delay assert of comparison to be able to cover lines even with Python 3.8
+    comparison_results = []
+    comparison_results.append(
+        compare_images("tests/visualize/reference/scan.png", str(fname), 0)
+    )
 
     # compare figure returned by function
     fname = tmp_path / "fig_from_return.png"
     fig.savefig(fname)
-    assert compare_images("tests/visualize/reference/scan.png", str(fname), 0) is None
+    comparison_results.append(
+        compare_images("tests/visualize/reference/scan.png", str(fname), 0)
+    )
 
     # do not save figure, but close it
     with mock.patch("cabinetry.visualize.utils._save_and_close") as mock_close_safe:
@@ -143,6 +155,9 @@ def test_scan(tmp_path):
             par_name, par_mle, par_unc, par_vals, par_nlls, close_figure=True
         )
         assert mock_close_safe.call_args_list == [((fig, None, True), {})]
+
+    for comp_res in comparison_results:
+        assert comp_res is None
 
     plt.close("all")
 
