07be58acc10885e4fa99235d9eb5db457a1bbbf0
Alexander Held <45009355+alexander-held@users.noreply.github.com>
2025-02-28 16:51:05 +0100

    fix typo

--- utils/create_ntuples.py
+++ utils/create_ntuples.py
@@ -157,7 +157,7 @@ def plot_distributions(data, weights, labels, pseudodata, bins):
     "--output_directory",
     type=str,
     default="inputs/",
-    help="Outpt directory for n-tuples to use in hello_world example. Useful for devs.",
+    help="Output directory for n-tuples to use in hello_world example. Useful for devs.",
 )
 def run(output_directory, *, visualize=False):
     # Create the output directory
